a7cbfa6a339a2a11dc33dada6564a1b25e6181a8
dineshgutta <dineshgutta@192.168.1.29>
2017-08-17 14:38:04 -0500

    first changes

--- src/main/java/com/srikanth/springboot/mvc/springbootmvc/SpringbootmvcApplication.java
+++ src/main/java/com/srikanth/springboot/mvc/springbootmvc/SpringbootmvcApplication.java
@@ -2,7 +2,7 @@ package com.srikanth.springboot.mvc.springbootmvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// this i sthe first application in git
 @SpringBootApplication
 public class SpringbootmvcApplication {
 
